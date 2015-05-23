292ade029d7f527f2776394ef7f0b67ce0154029
Oliver Joseph Ash <oliverjash@gmail.com>
2015-05-23 15:39:01 +0100

    Add EditorConfig

--- install-deps.sh
+++ install-deps.sh
@@ -49,6 +49,7 @@ brew install zsh
 sudo sh -c 'echo "/usr/local/bin/zsh" >> /etc/shells'
 chsh -s /usr/local/bin/zsh
 
+ln -s ~/Google\ Drive/Dotfiles/.editorconfig
 ln -s ~/Google\ Drive/Dotfiles/.gitconfig
 ln -s ~/Google\ Drive/Dotfiles/.gitignore
 ln -s ~/Google\ Drive/Dotfiles/.ssh
