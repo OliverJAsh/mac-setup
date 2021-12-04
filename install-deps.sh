d20953eb4862ff3c3c12680808e343527a2e02c0
Oliver Joseph Ash <oliverjash@gmail.com>
2021-12-04 16:23:57 +0000

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -33,22 +33,22 @@ brew install git
 
 brew install zsh-completions
 
-brew cask install dropbox
+brew install --cask dropbox
 echo "Open Dropbox for configuration"
 read
 
-brew cask install 1password
+brew install --cask 1password
 echo "Open 1Password, add vaults, and apply license"
 read
 
-brew cask install google-chrome
+brew install --cask google-chrome
 echo "Open Chrome and set up accounts."
 read
 
 echo "Set Chrome as default web browser"
 read
 
-brew cask install google-backup-and-sync
+brew install --cask google-backup-and-sync
 echo "Open Google Drive for configuration"
 read
 
@@ -62,15 +62,13 @@ read
 # https://github.com/yarnpkg/yarn/issues/630#issuecomment-277837483
 mkdir -p ~/.config
 
-ln -sf ~/Google\ Drive/Computer/Dotfiles/.gitconfig
-ln -sf ~/Google\ Drive/Computer/Dotfiles/.gitignore
-ln -sf ~/Google\ Drive/Computer/Dotfiles/.ssh
-ln -sf ~/Google\ Drive/Computer/Dotfiles/.zshrc
-ln -sf ~/Google\ Drive/Computer/Dotfiles/.gist
-ln -sf ~/Google\ Drive/Computer/Dotfiles/.unsplash-web-env ~/Development/unsplash-web/.env
-ln -sf ~/Google\ Drive/Computer/Dotfiles/.config/hub ~/.config/hub
-
-rm -rf ~/.bash*
+ln -sf ~/Google\ Drive/My\ Drive/Computer/Dotfiles/.gitconfig
+ln -sf ~/Google\ Drive/My\ Drive/Computer/Dotfiles/.gitignore
+ln -sf ~/Google\ Drive/My\ Drive/Computer/Dotfiles/.ssh
+ln -sf ~/Google\ Drive/My\ Drive/Computer/Dotfiles/.zshrc
+ln -sf ~/Google\ Drive/My\ Drive/Computer/Dotfiles/.gist
+ln -sf ~/Google\ Drive/My\ Drive/Computer/Dotfiles/.unsplash-web-env ~/Development/unsplash-web/.env
+ln -sf ~/Google\ Drive/My\ Drive/Computer/Dotfiles/.config/hub ~/.config/hub
 
 echo "Setup Google accounts on Mac for contacts and calendar"
 read
@@ -78,26 +76,10 @@ read
 brew install zsh-history-substring-search
 
 brew install nvm
-# As per `brew info nvm`
-mkdir ~/.nvm
 
 nvm install node # "node" is an alias for the latest version
 
 brew install yarn
-
-# This must be done before we create a dummy `/Applications/Xcode.app`,
-# otherwise `imagemagick` install will error.
-# https://github.com/vvo/gifify#requirements
-brew install ffmpeg
-brew install imagemagick
-brew install giflossy
-yarn global add gifify
-
-# Disable Spotlight search results for Developer
-# https://www.howtogeek.com/231829/how-to-disable-developer-search-results-in-spotlight-on-a-mac/
-# https://apple.stackexchange.com/a/181326/31346
-# Note this creates an error when running `brew doctor`: "The directory Xcode is reportedly installed to doesn't exist"
-touch /Applications/Xcode.app
 
 echo "Open Spotlight preferences and uncheck everything except Applications, Calculator and System Preferences"
 read
@@ -107,19 +89,13 @@ yarn global add pure-prompt
 echo "Open and close terminal"
 read
 
-brew cask install iterm2
+brew install --cask iterm2
 echo "Sync iTerm2 settings"
 read
 
 # https://docs.brew.sh/Shell-Completion#configuring-completions-in-zsh
 # https://stackoverflow.com/questions/13762280/zsh-compinit-insecure-directories#comment93718809_22753363
 chmod go-w "$(brew --prefix)/share"
-
-brew install hub
-echo "Authenticate Hub by attempting to talk to the API (lame)."
-read
-
-brew install gist
 
 brew install z
 
@@ -129,9 +105,9 @@ yarn global add is-online-cli
 
 brew install tree
 
-yarn global add diff-so-fancy
+brew install git-delta
 
-brew cask install visual-studio-code
+brew install --cask visual-studio-code
 
 echo "Open and Close VS Code"
 read
@@ -160,7 +136,7 @@ heroku plugins:install heroku-accounts
 heroku accounts:add personal
 heroku accounts:add unsplash
 
-brew cask install spectacle
+brew install --cask spectacle
 echo "Open and close Spectacle"
 read
 
@@ -180,14 +156,10 @@ cd -
 
 brew install jq
 
-brew install travis
-
-brew cask install spotify
+brew install --cask spotify
 
 echo "Open Spotify and login"
 read
-
-brew install bluetoothconnector
 
 yarn global add yarn-deduplicate
 
@@ -195,28 +167,6 @@ yarn global add np
 
 npm login
 
-brew cask install zoomus
+brew install --cask zoom
 echo "Open Zoom, login"
 read
-
-brews cask install keycastr
-echo "Open KeyCastr"
-read
-
-brew cask install android-platform-tools
-
-brew cask install ngrok
-
-# https://stackoverflow.com/questions/24342886/how-to-install-java-8-on-mac/32811065#32811065
-# TODO
-# https://github.com/AdoptOpenJDK/homebrew-openjdk/issues/106
-# brew tap adoptopenjdk/openjdk
-brew cask install adoptopenjdk8
-
-brew cask install adobe-creative-cloud
-echo "Sign into Adobe CC, install apps"
-read
-
-# For Selenium (?)
-brew cask install java8
-
