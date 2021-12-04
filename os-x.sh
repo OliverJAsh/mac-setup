9ae29f533d425231505d58ca16e83a10ee5a4e3d
Oliver Joseph Ash <oliverjash@gmail.com>
2021-12-04 13:36:24 +0000

    Update os-x.sh

--- os-x.sh
+++ os-x.sh
@@ -23,21 +23,9 @@
 # - Enable three finger drag
 # - https://support.apple.com/en-gb/guide/mac-help/mh40596/mac
 
-# Download Spotify playlists
-
-# Disable shadow in screenshots
-# https://www.macgasm.net/2011/05/23/disable-dropshadow-mac-os-window-screenshots/
-defaults write com.apple.screencapture disable-shadow -bool true
-killall SystemUIServer
-
 # Remove all apps from dock
 # http://krypted.com/mac-security/remove-all-apps-from-the-dock-in-os-x/
 defaults write com.apple.dock persistent-apps -array
 # Remove all folders from dock
 defaults write com.apple.dock persistent-others -array
 killall Dock
-
-echo "Set user avatar"
-read
-
-echo "Create shortcut for sleep. See https://www.quora.com/Is-there-a-keyboard-shortcut-to-put-the-2016-MacBook-pro-to-sleep/answer/Ephraim-Rothschild."
